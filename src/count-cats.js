3e0474e8bf3719d2062795f76c6c337a098571b7
Svetrima <svetsmetanina@gmail.com>
2020-02-21 22:02:33 +0300

    feat:complete count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(kitty) {
+ // throw 'Not implemented';
   // remove line with error and write your code here
+   return kitty.flat().filter(ears=>ears==='^^').length;
 };
