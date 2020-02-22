8619f88f8c900b267c69a0539c54644f4c04b30a
Svetrima <svetsmetanina@gmail.com>
2020-02-22 12:18:02 +0300

    feat:complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //ln(N/N0)/k=t
+  let valueCarbon = parseFloat(sampleActivity);
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  if(sampleActivity == null || valueCarbon<=0 ||valueCarbon>MODERN_ACTIVITY || !valueCarbon || typeof sampleActivity !='string'){
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / valueCarbon) / k);
 };
