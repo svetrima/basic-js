58ffd0720ca6e835deb0805832c5106ff9f59efc
Svetrima <svetsmetanina@gmail.com>
2020-03-22 17:43:47 +0300

    feat:add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    // throw 'Not implemented';
+    // remove line with error and write your code here
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    let filterMem = members.filter(value => {
+        if (typeof value === 'string') {
+            return value;
+        }
+    }).map(word => {
+        return word.trim()[0].toUpperCase(); // cut zero index
+    }).sort().join('');
+    return filterMem;
 };
